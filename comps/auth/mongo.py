fb0d2026e4514133c3ff6025033b8ac854e75945
Het Naik <awesomehet@gmail.com>
2025-03-31 17:36:29 +0530

    auth backend

---
+++ comps/auth/mongo.py
@@ -0,0 +1,20 @@
+import uuid
+from passlib.context import CryptContext
+from comps.mongo_client import mongo_client
+
+collection = mongo_client['easy-circulars']['authentication']
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+NAMESPACE_UUID = uuid.UUID("8d5decaf-e37c-4ecc-919e-09d2721b6ea0")
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = pwd_context.hash(password)
+        self._id = str(uuid.uuid5(namespace=NAMESPACE_UUID, name=self.username))
+
+    def register(self):
+        if collection.find_one({"username": self.username}) is None:
+            collection.insert_one({"_id": self._id, "username": self.username, "password": self.password})
+            return "Registered"
+
+        return "Already registered"
